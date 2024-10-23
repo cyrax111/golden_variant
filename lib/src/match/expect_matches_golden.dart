83f3974cf0e43171f53be44326418842d2207459
cyrax <bskaxe@gmail.com>
2024-10-23 10:23:11 +0400

    feat: move path to separate pathBuilder

--- lib/src/match/expect_matches_golden.dart
+++ lib/src/match/expect_matches_golden.dart
@@ -1,27 +1,48 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_variant/golden_variant.dart';
 
-Future<void> expectMatchesGoldenFile<T>({
-  ValueVariantComposite? variant,
-  String? path,
+Future<void> expectMatchesGoldenFile<T>(
+  GoldenFilePathBuilder pathBuilder, {
   Finder? finder,
-  String? suffix,
   String? reason,
   dynamic skip,
   int? version,
 }) async {
-  final pathStr = path == null ? '' : '$path/';
-  final variantStr = variant == null ? '' : '${variant.currentValue}';
-  final suffixStr = suffix ?? '';
-
   Finder? finderByTType;
   if (T != dynamic) {
     finderByTType = find.byType(T);
   }
   await expectLater(
     finder ?? finderByTType ?? find.byType(DeviceFrameWrapper),
-    matchesGoldenFile('preview/$pathStr$variantStr$suffixStr.png', version: version),
+    matchesGoldenFile(pathBuilder(), version: version),
     reason: reason,
     skip: skip,
   );
+}
+
+abstract interface class GoldenFilePathBuilder {
+  String call();
+}
+
+class DefaultPath implements GoldenFilePathBuilder {
+  const DefaultPath({
+    required this.variant,
+    this.path,
+    this.suffix,
+    this.prefix,
+  });
+
+  final ValueVariantComposite variant;
+  final String? path;
+  final String? suffix;
+  final String? prefix;
+
+  @override
+  String call() {
+    final variantStr = variant.currentValue.join('-').replaceAll(' ', '_');
+    final suffixStr = suffix ?? '';
+    final prefixStr = prefix == null ? '' : '$prefix-';
+    final pathStr = path == null ? (prefix == null ? '' : '$prefix/') : '$path/';
+    return 'preview/$pathStr$prefixStr[$variantStr]$suffixStr.png';
+  }
 }
