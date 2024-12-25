6ddd3cae5e446de763e49256974a529395d12658
cyrax <bskaxe@gmail.com>
2024-12-25 22:46:07 +0400

    feat: add AwaitImages

--- lib/golden_variant.dart
+++ lib/golden_variant.dart
@@ -1,4 +1,4 @@
-library golden_variant;
+library;
 
 export 'package:golden_variant/src/predefined_variant/device_variant.dart';
 export 'package:golden_variant/src/predefined_variant/locale_variant.dart';
@@ -9,5 +9,6 @@ export 'package:golden_variant/src/tool/device_frame_wrapper.dart';
 export 'package:golden_variant/src/variant_composite/value_variant_composite.dart';
 export 'package:golden_variant/src/variant_composite/test_variant_composite.dart';
 export 'package:golden_variant/src/match/expect_matches_golden.dart';
+export 'package:golden_variant/src/tool/await_images.dart';
 
 export 'package:device_frame_plus/device_frame_plus.dart';
